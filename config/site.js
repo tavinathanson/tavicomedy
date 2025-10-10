51b0ca2333a7383f2acab6d336dffed666fa0440
Tavi Nathanson <tavi.nathanson@gmail.com>
2025-10-09 22:06:27 -0400

    Start selling tickets for next show

--- config/site.js
+++ config/site.js
@@ -2,10 +2,10 @@
 
 export const siteConfig = {
   // Set to true when tickets are on sale, false to show mailing list signup
-  showcaseTicketsAvailable: false,
+  showcaseTicketsAvailable: true,
   
   // When showcaseTicketsAvailable is true, set the show date
-  nextShowDate: "Saturday, February 22", // Update this when you have a date!
+  nextShowDate: "Saturday, November 15", // Update this when you have a date!
   
   // Open mic date
   nextOpenMicDate: "Thursday, October 16, 2025",

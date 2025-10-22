b7c21d99d745e61e82807f03b47a18f13d846b34
Tavi Nathanson <tavi.nathanson@gmail.com>
2025-10-22 09:09:41 -0400

    Add Clarity

--- pages/_document.js
+++ pages/_document.js
@@ -42,6 +42,22 @@ export default function Document() {
           src="https://www.eventbrite.com/static/widgets/eb_widgets.js"
         />
         {/* End Eventbrite Widget */}
+
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tu66fscdyq");
+            `,
+          }}
+        />
+        {/* End Microsoft Clarity */}
       </Head>
       <body>
         <Main />

74793a41809b7b79566da3f21e020b4621e71b3a
Tavi Nathanson <tavi.nathanson@gmail.com>
2026-05-01 15:35:23 -0400

    Update mic date

--- config/site.js
+++ config/site.js
@@ -33,7 +33,7 @@ export const siteConfig = {
   get nextUpcomingShowDate() { return this.nextUpcomingShowDateISO ? formatDisplayDate(this.nextUpcomingShowDateISO) : null },
 
   // Open mic date in YYYY-MM-DD format
-  nextOpenMicDateISO: "2026-04-30",
+  nextOpenMicDateISO: "2026-05-28",
   get nextOpenMicDate() { return formatDisplayDate(this.nextOpenMicDateISO) },
 
   // No tickets available mode (when showcaseTicketsAvailable is false)
